5a131f8ae0400e4f6aa6529b3fe9c119eaa6a7c3
dekum <withswiftness@gmail.com>
2020-02-09 10:47:10 -0500

    Initial commit

---
+++ Test/PersonTest.java
@@ -0,0 +1,60 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import sample.Person;
+
+public class PersonTest
+{
+  //public Person(String firstName, String lastName, String address,
+    // String city, String state, String zip, String phone)
+  Person test_Person = new Person("John","Doe","123 fake street","Fort Myers","FL","33901","123456789");
+
+  //testing successful conditon
+  @Test
+  public void getFirstNameSuccess() {
+    Person test_Person = new Person("John","Doe","123 fake street","Fort Myers","FL","33901","123456789");
+    assertEquals("John",test_Person.getFirstName());
+    System.out.println(test_Person.getFirstName());
+  }
+  //This test should fail
+  @Test
+  public void getFirstNameFail() {
+    assertEquals("Michael",test_Person.getFirstName());
+  }
+
+  @Test
+  public void getLastName() {
+  }
+
+  @Test
+  public void getAddress() {
+  }
+
+  @Test
+  public void getCity() {
+  }
+
+  @Test
+  public void getState() {
+  }
+
+  @Test
+  public void getZip() {
+  }
+
+  @Test
+  public void getPhone() {
+  }
+
+//  @Test
+//  public void toString() {
+//  }
+
+  @Test
+  public void containsString() {
+  }
+
+  @Test
+  public void getField() {
+  }
+}
